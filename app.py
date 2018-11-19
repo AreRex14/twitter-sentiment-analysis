d35f0e1a21fb2f42129e195c8483173adf2599d7
arerex14 <arerifxynwa@gmail.com>
2018-11-19 13:35:10 +0800

    little change to visualize on index.html and comment in malaya not working poblem

--- app.py
+++ app.py
@@ -7,15 +7,15 @@ import random
 import time
 
 # Malaya NLTK
-import malaya
+# import malaya
 
-multinomial = malaya.multinomial_detect_languages()
-bayes_sentiment = malaya.pretrained_bayes_sentiment()
+# multinomial = malaya.multinomial_detect_languages()
+# bayes_sentiment = malaya.pretrained_bayes_sentiment()
 
 app = Flask(__name__)
 Bootstrap(app)
 
-@app.route('/')
+@app.route('/' , methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
@@ -26,12 +26,15 @@ def analyse():
     if request.method == 'POST': 
         try:
             rawtext = request.form['rawtext']
-            lang_predict = multinomial.predict(rawtext)
+            # lang_predict = multinomial.predict(rawtext)
             blob = TextBlob(rawtext)
-            if lang_predict != 'ENGLISH':
+            blob_lang = blob.detect_language()
+            # if lang_predict != 'ENGLISH':
+            #   en_blob = blob.translate(to='en')
+            # else:
+            #   en_blob = blob
+            if blob_lang != 'en':
                en_blob = blob.translate(to='en')
-            else:
-               en_blob = blob
 
             pass
         except exceptions.NotTranslated as NT:
@@ -42,10 +45,11 @@ def analyse():
 				    # NLP stuff
             blob_sentiment, blob_subjectivity = en_blob.sentiment.polarity, en_blob.sentiment.subjectivity
             number_of_tokens = len(list(en_blob.words))
-
-            malaya_sentiment = bayes_sentiment.predict(rawtext)
-            malaya_proba = bayes_sentiment.predict(rawtext, get_proba=True)
-            proba_value = malaya_proba.get(malaya_sentiment)
+            
+            # if lang_predict == 'MALAY':
+            #   malaya_sentiment = bayes_sentiment.predict(rawtext)
+            #   malaya_proba = bayes_sentiment.predict(rawtext, get_proba=True)
+            #   proba_value = malaya_proba.get(malaya_sentiment)
 				    # Extracting Main Points
             nouns = list()
             for word, tag in en_blob.tags:
@@ -60,10 +64,11 @@ def analyse():
                        summary = final_word
                        end = time.time()
                        final_time = end-start
-            if lang_predict != 'ENGLISH':
-               return render_template('index.html', status = error, received_text = rawtext, number_of_tokens = number_of_tokens, blob_sentiment = proba_value, blob_subjectivity = blob_subjectivity, summary = summary, final_time = final_time)
-            else:
-               return render_template('index.html', status = error, received_text = rawtext, number_of_tokens = number_of_tokens, blob_sentiment = blob_sentiment, blob_subjectivity = blob_subjectivity, summary = summary, final_time = final_time)
+            # fix malaya sentiment not working
+            # if lang_predict == 'MALAY':
+            #   return render_template('index.html',  status = error, received_text = rawtext, number_of_tokens = number_of_tokens, blob_sentiment = proba_value, blob_subjectivity = blob_subjectivity, summary = summary, final_time = final_time)
+            # else:
+            return render_template('index.html', status = error, received_text = rawtext, number_of_tokens = number_of_tokens, blob_sentiment = blob_sentiment, blob_subjectivity = blob_subjectivity, summary = summary, final_time = final_time)
             pass
             
 if __name__ == '__main__':
